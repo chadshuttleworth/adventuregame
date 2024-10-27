f23eb6a21d9092f39bdf4633d88cf679cf72db46
cloudlord301 <cloudlord115@gmail.com>
2024-10-28 10:52:13 +1100

    Add files via upload

---
+++ adventue.py
@@ -0,0 +1,41 @@
+print ("hello adventures")
+print ("welcome to westwood shire taven")
+name = input ("dear traveler what is your name: ")
+
+inv_list = [ "10 gold","key", "food"]
+player = inv_list [1]
+
+if name == "ALESHA" or name == "SMUDGE":
+   alesha = input.upper("that name rings a bell")
+
+answer = input( ( name + ",thx you do you want a drink"))
+
+if answer=="no":
+    print (" ah what a shame" )
+    exit ("you travel along your day nornamlly")
+else:
+    print (" no troubles we well get that " +name )
+    
+print (" here is your ale ")
+
+print ( name + ",you feel very dizzy now ")
+
+print ( " you have fainted now !!! ")
+
+print("..........................................")
+
+
+
+
+
+
+print (" ..........................your ,finally, awake")
+
+weapon = "bow, axe , sword"
+
+order = input ("what weapon do you like here our list is, "+ weapon )
+print ("this is your invertory")
+invertory = inv_list
+
+print (invertory)
+print (weapon) 
